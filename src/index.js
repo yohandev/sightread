6295a78c9fc17d9d29e5433091f0de122d829c0c
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-04-24 14:17:46 -0700

    wasm-rs <--> js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import { add } from '../rs/src/lib.rs';
+import * as rs from '../rs/src/lib.rs';
 
-console.log(`3 + 2 = ${add(3, 2)}`);
+console.log(`3 + 2 = ${rs.add(3, 2)}`);
+console.log(rs.memory);
+
+const arr = new Int32Array(rs.memory.buffer, 0, 5);
+
+arr.set([1, 2, 3, 4, 5]);
+
+console.log(`sum([1, 2, 3, 4, 5]) = ${rs.sum(0, 5)}`);
+
+rs.inc(0, 5);
+console.log(`inc([1, 2, 3, 4, 5]) = ${arr.slice(0, 5)}`);
+
+console.log(rs.memory);
