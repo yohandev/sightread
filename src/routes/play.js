4a2099e5d633061eadce4b2398c9ef4fcb9401ce
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-04-25 07:36:33 -0700

    note render

--- src/routes/play.js
+++ src/routes/play.js
@@ -4,6 +4,20 @@ import microphone from '../mic';
 import crate from '../crate';
 import draw from '../draw';
 
+const align = (note) =>
+{
+    switch (note.charAt(0))
+    {
+        case 'G': return 3;
+        case 'F': return 11;
+        case 'E': return 18;
+        case 'D': return 25;
+        case 'C': return 32;
+        case 'B': return 39;
+        case 'A': return 46;
+    }
+}
+
 const play =
 {
     view: () =>
@@ -13,15 +27,19 @@ const play =
             {/* { play.state['A4'] ? (<h1>You played an A4! Not so stupid after all</h1>) : null } */}
             <svg id='notes-container'>'
                 { draw.treble }
-                <g class='notes-down' style={`transform: translate(1.25em, ${3}%)`}>
-                    { draw.note_down }
-                </g>
+                {
+                    play.state.notes.map(note =>
+                        <g class='notes-down' style={`transform: translate(1.25em, ${align(note)}%)`}>
+                            { draw.note_down }
+                        </g>
+                )}
             </svg>
         </div>
     ),
     state:
     {
         'A4': false,
+        notes: ['A4', 'C#5']
     }
 }
 
